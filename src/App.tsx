eec7404dac63569117ad083c2914b07936f7de5b
George English <gaenglish@gmail.com>
2023-04-06 13:10:14 -0400

    implementing background component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 // @ts-ignore
 import bg from "./assets/grid_0.png";
 import { Resume } from "./pages/Resume";
 import { NavBar } from "./components/navigation/NavBar";
 import { DarkTheme, LightTheme } from "./themes";
-import { ThemeProvider } from "@uni-design-system/uni-react";
+import { Background, ThemeProvider } from "@uni-design-system/uni-react";
 
 function App() {
   const [theme, setTheme] = useState(DarkTheme);
@@ -20,22 +19,19 @@ function App() {
   return (
     // @ts-ignore
     <ThemeProvider theme={theme}>
-      <div
-        className="App"
-        style={{
-          backgroundImage: `url(${bg})`,
-          backgroundAttachment: "fixed",
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          minHeight: "100vh",
-        }}
+      <Background
+        imageUrl={bg}
+        attachment="fixed"
+        position="center"
+        size="cover"
+        minHeight="100vh"
       >
         <NavBar onThemeChange={handleThemeChange} />
         <Routes>
           <Route path="/" element={<Resume />} />
           <Route path="/resume" element={<Resume />} />
         </Routes>
-      </div>
+      </Background>
     </ThemeProvider>
   );
 }
